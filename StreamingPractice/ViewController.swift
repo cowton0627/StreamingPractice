cfac28540997b530bffa03ea4ca84ff7849ac5b5
鄭淳澧 <cowton0517@gmail.com>
2021-12-22 18:31:05 +0800

    Initial Commit

---
+++ StreamingPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreamingPractice
+//
+//  Created by Chun-Li Cheng on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
