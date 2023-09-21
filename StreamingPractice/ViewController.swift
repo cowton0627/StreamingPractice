a57427218b66e082d56d5bb9dc937e5a36649f8e
cowton0627 <83654992+cowton0627@users.noreply.github.com>
2023-09-21 15:56:04 +0800

    2023 0921 1555

--- StreamingPractice/ViewController.swift
+++ StreamingPractice/ViewController.swift
@@ -9,67 +9,60 @@ import UIKit
 import StreamingKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var urlTxtField: UITextField!
-//    @IBOutlet weak var playBtn: UIButton!
-//    @IBOutlet weak var pauseBtn: UIButton!
-//    @IBOutlet weak var clearBtn: UIButton!
-    @IBOutlet weak var grayView: UIView!
     
-//    lazy var myLabel: UILabel = {
-//        let l = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-//        view.addSubview(l)
-//        l.translatesAutoresizingMaskIntoConstraints = false
-//        return l
+    /// 測試 Localized String & 多國語系
+//    private lazy var myLabel: UILabel = {
+//        let label = UILabel(frame: CGRect(x: 150, y: 650, width: 200, height: 40))
+//        view.addSubview(label)
+////        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
 //    }()
     
+    /// 串流播放器
     private let videoPlayer = StreamingVideoPlayer()
-//    private var isPlaying = false
+    
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var videoView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 //        myLabel.text = NSLocalizedString("TestString", comment: "你好嗎？")
-//        myLabel.center = view.center
         
-        urlTxtField.attributedPlaceholder = NSAttributedString.init(string: "Enter url here", attributes: [
-            .font: UIFont.systemFont(ofSize: 20, weight: .light),
-            .foregroundColor: UIColor.darkGray
-        ])
+//        urlTextField.attributedPlaceholder =
+//        NSAttributedString.init(string: "Please Enter URL",
+//                                attributes: [
+//            .font: UIFont.systemFont(ofSize: 18, weight: .light),
+//            .foregroundColor: UIColor.darkGray
+//        ])
         
         setupVideoPlayer()
     }
     
     private func setupVideoPlayer() {
-        videoPlayer.add(to: grayView)
+        videoPlayer.add(to: videoView)
     }
     
-    @IBAction func txtFieldDidEnd(_ sender: UITextField) {
-//        guard let text = urlTxtField.text else {
-//            return
-//        }
-//        currentTitle = text
-    }
-    @IBAction func playTapped() {
-        guard let text = urlTxtField.text, let url = URL(string: text) else {
+    
+    @IBAction func playBtnTapped() {
+        guard let text = urlTextField.text,
+              !text.isEmpty,
+              let url = URL(string: text) else {
             print("Error parsing URL")
             return
         }
-        if !(videoPlayer.isPlaying ?? true) {
+        
+        if videoPlayer.isPlaying == false {
             videoPlayer.play(url: url)
         }
-//        if !videoPlayer.avPlayer.isPlaying {
-//            //        if !isPlaying {
-//                        videoPlayer.play(url: url)
-//            //        }
-//            //        isPlaying = true
-//        }
-
+        
     }
-    @IBAction func pauseTapped() {
+    
+    @IBAction func pauseBtnTapped() {
         videoPlayer.pause()
-//        isPlaying = false
     }
-    @IBAction func clearTapped() {
-        urlTxtField.text = nil
+    
+    @IBAction func clearBtnTapped() {
+        urlTextField.text = nil
         videoPlayer.pause()
     }
 
